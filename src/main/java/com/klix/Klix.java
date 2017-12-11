333c2381a0c1d881b7fa5cbd8b2bac399cbc3a1d
Yoaz Menda <yoazmenda@gmail.com>
2017-12-11 12:15:33 +0200

    added procfile

--- src/main/java/com/klix/Klix.java
+++ src/main/java/com/klix/Klix.java
@@ -1,3 +1,5 @@
+package com.klix;
+
 import static spark.Spark.get;
 import static spark.Spark.port;
 
@@ -7,7 +9,7 @@ public class Klix implements Runnable {
         if (portValue == null || portValue.equals("")){
             portValue = "443";
         }
-        int port = Integer.valueOf(System.getProperty("PORT"));
+        int port = Integer.valueOf(portValue);
 
         port(port);
         get("/hello", (req, res) -> "Hello World");
