252e4eec4d988c686cfe9b8a56a0e9ad27f905b6
Yoaz Menda <yoazmenda@gmail.com>
2017-12-11 11:31:13 +0200

    added port

--- src/main/java/Klix.java
+++ src/main/java/Klix.java
@@ -1,7 +1,15 @@
 import static spark.Spark.get;
+import static spark.Spark.port;
 
 public class Klix implements Runnable {
     public void run() {
+        String portValue = System.getProperty("PORT");
+        if (portValue == null || portValue.equals("")){
+            portValue = "443";
+        }
+        int port = Integer.valueOf(System.getProperty("PORT"));
+
+        port(port);
         get("/hello", (req, res) -> "Hello World");
     }
 
