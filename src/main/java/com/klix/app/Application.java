40a42e2afeb20d14663e5031bdf6b66533789440
Yoaz Menda <yoazmenda@gmail.com>
2017-12-13 15:51:01 +0200

    changed to port from env var

--- src/main/java/com/klix/app/Application.java
+++ src/main/java/com/klix/app/Application.java
@@ -4,19 +4,27 @@ package com.klix.app;/*
 
 
 import com.klix.app.link.LinkDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spark.Spark;
+
+import java.util.logging.Level;
+
 
 import static spark.Spark.post;
 
 
 public class Application {
 
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static LinkDao linkDao;
 
     public static void main(String[] args) {
         int port = 443;
-        String portStr = System.getProperty("PORT");
+        String portStr = System.getenv("PORT");
         if (portStr != null && !portStr.equals("")) {
+            log.info("using port from environment: " + port);
             port = Integer.valueOf(portStr);
         }
 
