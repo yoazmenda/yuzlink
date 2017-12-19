42673faa087cc51cb17a0a5ab2c341e16d65db88
Yoaz Menda <yoazmenda@gmail.com>
2017-12-19 15:56:23 +0200

    testing heroku postgres

--- src/main/java/com/klix/app/Application.java
+++ src/main/java/com/klix/app/Application.java
@@ -6,10 +6,16 @@ import com.klix.app.db.Model;
 import com.klix.app.services.link.RedirectHandler;
 import com.klix.app.services.link.ShortenHandler;
 import com.klix.app.utils.CommandLineOptions;
+import com.sun.org.apache.bcel.internal.generic.Select;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 import spark.Spark;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import static spark.Spark.*;
 
@@ -18,7 +24,7 @@ public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException {
         CommandLineOptions options = new CommandLineOptions();
         new JCommander(options, args);
         log.info("Options.debug = " + options.debug);
@@ -31,10 +37,25 @@ public class Application {
         port(options.servicePort);
 
 
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+
         Sql2o sql2o = new Sql2o(
-                "jdbc:postgresql://" + options.dbHost + ":" + options.dbPort + "/" + options.database,
-                options.dbUsername,
-                options.dbPassword);
+                dbUrl,
+                username,
+                password);
+
+        try(org.sql2o.Connection cn = sql2o.open()){
+            cn.createQuery("select * from roles").executeAndFetch(Object.class);
+        } catch (Exception ex){
+            ex.printStackTrace();
+            halt("Can't connect to db: ");
+
+        }
 
         Model model = new KlixModel(sql2o);
 
