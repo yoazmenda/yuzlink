bb385316292dbfaa656c3ccff446bb505f1f2016
Yoaz Menda <yoazmenda@gmail.com>
2017-12-24 23:04:40 +0200

    new css-grid design

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,5 +1,25 @@
-$(document).ready(function(){
-    $('#textBox').on('input change', function (){
+$(document).ready(function () {
+
+
+    $('#textBox').on('input change', function () {
         $('#button').attr('disabled', $(this).val() == '');
-    })
+    });
+
+    $('.menu-item').click(function () {
+        var activeElement = this;
+        requestName = $(this).attr('name');
+        $('.content').load(requestName + ".html", function (responseTxt, statusTxt, xhr) {
+                if (statusTxt == "success") {
+                    $(".menu-item").removeClass('active');
+                    $(activeElement).addClass('active');
+                }
+            }
+        )
+    });
+
+    $('.content').load(
+        "home.html"
+    )
+
+
 });
